b68334b3bf3e534ab677641a86d72e6ebfe48a75
rassael <rassael@lightricks.com>
2022-10-06 11:33:52 +0300

    Test Repository, database, network error and getItems of ViewModel

--- app/src/androidTest/java/com/lightricks/feedexercise/data/FeedRepositoryTest.kt
+++ app/src/androidTest/java/com/lightricks/feedexercise/data/FeedRepositoryTest.kt
@@ -1,13 +1,138 @@
 package com.lightricks.feedexercise.data
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.Observer
+import android.content.Context
+import android.util.Log
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.lightricks.feedexercise.database.FeedDatabase
+import com.lightricks.feedexercise.database.FeedItemEntity
+import com.lightricks.feedexercise.network.FeedApiService
+import com.lightricks.feedexercise.network.GetFeedResponse
+import com.lightricks.feedexercise.network.ItemDto
+import com.lightricks.feedexercise.ui.feed.FeedViewModel
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
-import java.util.concurrent.CountDownLatch
-import java.util.concurrent.TimeUnit
+import java.io.IOException
+
 
 @RunWith(AndroidJUnit4::class)
 class FeedRepositoryTest {
-   //todo: add the tests here
+
+    private val context: Context = ApplicationProvider.getApplicationContext<Context>()
+    private val allDaoItems: List<ItemDto> = fakeFeedResponse().itemDto
+    private val allFeedEntities: List<FeedItemEntity> = allDaoItems.toEntities()
+    private val emptyFeedItemsList: List<FeedItem> = emptyList()
+    private lateinit var testDatabase: FeedDatabase
+    private lateinit var testService: TestService
+    private lateinit var testRepository: Repository
+    private lateinit var testViewModel: FeedViewModel
+
+
+    @Before
+    fun repoCreation() {
+        testDatabase = Room.inMemoryDatabaseBuilder(context, FeedDatabase::class.java).build()
+        testService = TestService()
+        testRepository = FeedRepository(testDatabase, testService)
+        testViewModel = FeedViewModel(testRepository)
+    }
+
+    @Test
+    fun testRefresh_emptyWebsite_successGet() = runBlocking {
+        val itemsBeforeRefresh: List<FeedItemEntity> = testDatabase.feedDao.getAll().first()
+        testService.setItems(emptyList())
+        testRepository.refresh()
+        val itemsAfterRefresh: List<FeedItemEntity> = testDatabase.feedDao.getAll().first()
+        assertEquals(emptyFeedItemsList, itemsBeforeRefresh)
+        assertEquals(itemsAfterRefresh.toFeedItems(), emptyFeedItemsList)
+    }
+
+    @Test
+    fun testRefresh_fullWebsite_successGet() = runBlocking {
+        val itemsBeforeRefresh: List<FeedItemEntity> = testDatabase.feedDao.getAll().first()
+        testService.setItems(allDaoItems)
+        testRepository.refresh()
+        val itemsAfterRefresh: List<FeedItemEntity> = testDatabase.feedDao.getAll().first()
+        assertEquals(emptyFeedItemsList, itemsBeforeRefresh)
+        assertEquals(allFeedEntities, itemsAfterRefresh)
+    }
+
+    @Test
+    fun testRefresh_emptyAfterFull_successGet() = runBlocking {
+        testService.setItems(allDaoItems)
+        testRepository.refresh()
+        testService.setItems(emptyList())
+        testRepository.refresh()
+        val itemsAfterDeleting: List<FeedItemEntity> = testDatabase.feedDao.getAll().first()
+        assertEquals(emptyFeedItemsList, itemsAfterDeleting)
+    }
+
+    @Test
+    fun testRefresh_removeFirstItems_successGet() = runBlocking {
+        testService.setItems(allDaoItems)
+        testRepository.refresh()
+        testService.setItems(allDaoItems.drop(1))  // remove first element
+        testRepository.refresh()
+        val itemsAfterDeleting: List<FeedItemEntity> = testDatabase.feedDao.getAll().first()
+        assertEquals(allFeedEntities.drop(1), itemsAfterDeleting)
+    }
+
+    @Test
+    fun testRefresh_currentEmpty_GetFailure() = runBlocking {
+        testService.setItems(emptyList())
+        testRepository.refresh()
+        testService.returnError()  // now fail
+        try {
+            testRepository.refresh() }
+        catch (e : IOException) {
+            Log.d("networkErr", "error") }  // supposed to fail
+        val currentItems: List<FeedItemEntity> = testDatabase.feedDao.getAll().first()
+        assertEquals(listOf<FeedItemEntity>(), currentItems)
+    }
+
+    @Test
+    fun testRefresh_currentNotEmpty_GetFailure() = runBlocking {
+        testService.setItems(allDaoItems)
+        testRepository.refresh()
+        testService.returnError()  // now fail
+        try {
+            testRepository.refresh() }
+        catch (e : IOException) {
+            Log.d("networkErr", "error") }  // supposed to fail
+        val currentItems: List<FeedItemEntity> = testDatabase.feedDao.getAll().first()
+        assertEquals(allFeedEntities ,currentItems)
+    }
+
+    private fun fakeFeedResponse(): GetFeedResponse {
+        val moshi = Moshi.Builder().build()
+        val adapter: JsonAdapter<GetFeedResponse> = moshi.adapter(GetFeedResponse::class.java)
+        val jsonName = "get_feed_response.json"
+        val jsonFile = context.assets.open(jsonName).bufferedReader().use { it.readText() }
+        return adapter.fromJson(jsonFile)!!
+    }
+}
+
+private class TestService : FeedApiService {
+
+    private var serviceItems : List<ItemDto> = emptyList()
+    private var isSuccess : Boolean = true
+
+    fun returnSuccess() { isSuccess = true }
+    fun returnError() { isSuccess = false }
+
+    fun setItems(newList : List<ItemDto>) { serviceItems = newList }
+
+    override suspend fun getFeedItems(): GetFeedResponse {
+        return if (isSuccess) {
+            GetFeedResponse(serviceItems)
+        } else {
+            throw IOException()
+        }
+    }
 }
---
+++ app/src/test/java/com/lightricks/feedexercise/data/FeedViewModelTest.kt
@@ -0,0 +1,204 @@
+package com.lightricks.feedexercise.data
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import com.lightricks.feedexercise.network.GetFeedResponse
+import com.lightricks.feedexercise.ui.feed.FeedViewModel
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.test.*
+import org.junit.*
+import org.junit.rules.TestRule
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import org.mockito.Mockito.*
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeoutException
+
+@RunWith(JUnit4::class)
+class FeedViewModelTest {
+
+    private val allFeedItems: List<FeedItem> = getAllItems()
+
+    // need to use test dispatcher because we can't observe on the main dispatcher.
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    // for getting liveData's value in the test
+    @get:Rule var rule: TestRule = InstantTaskExecutorRule()
+
+    @Test
+    fun testGetItems_emptyList(): Unit = runTest {
+        val repository: Repository = mock(Repository::class.java)
+        `when`(repository.getItems()).thenReturn(flow { emit(emptyList()) })
+        `when`(repository.refresh()).thenReturn(Unit)
+        val viewModel = FeedViewModel(repository)
+        Assert.assertEquals(listOf<FeedItem>(), viewModel.getFeedItems().getOrAwaitValue())
+    }
+
+    @Test
+    fun testGetItems_notEmptyList(): Unit = runTest {
+        val repository : Repository = mock(Repository::class.java)
+        `when`(repository.getItems()).thenReturn(MutableStateFlow(allFeedItems))
+        `when`(repository.refresh()).then {  }
+        val viewModel = FeedViewModel(repository)
+        Assert.assertEquals(allFeedItems, viewModel.getFeedItems().getOrAwaitValue())
+    }
+}
+
+fun <T> LiveData<T>.getOrAwaitValue(
+    time: Long = 2,
+    timeUnit: java.util.concurrent.TimeUnit = java.util.concurrent.TimeUnit.SECONDS,
+    afterObserve: () -> Unit = {}
+): T {
+    var data: T? = null
+    val latch = CountDownLatch(1)
+    val observer = object : Observer<T> {
+        override fun onChanged(o: T?) {
+            data = o
+            latch.countDown()
+            this@getOrAwaitValue.removeObserver(this)
+        }
+    }
+    this.observeForever(observer)
+    afterObserve.invoke()
+    // Don't wait indefinitely if the LiveData is not set.
+    if (!latch.await(time, timeUnit)) {
+        this.removeObserver(observer)
+        throw TimeoutException("LiveData value was never set.")
+    }
+    @Suppress("UNCHECKED_CAST")
+    return data as T
+}
+
+fun getAllItems(): List<FeedItem> {
+    val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+    val adapter: JsonAdapter<GetFeedResponse> = moshi.adapter(GetFeedResponse::class.java)
+    return adapter.fromJson("{\n" +
+            "  \"templatesMetadata\": [\n" +
+            "    {\n" +
+            "      \"configuration\": \"lensflare-unleash-the-power-of-nature.json\",\n" +
+            "      \"id\": \"01E18PGE1RYB3R9YF9HRXQ0ZSD\",\n" +
+            "      \"isNew\": false,\n" +
+            "      \"isPremium\": true,\n" +
+            "      \"templateCategories\": [\n" +
+            "        \"01DJ4TM160ETZR0NT4HA2M0ZTK\",\n" +
+            "        \"01DJ4TM161MRR86QFAXJTWP7NM\"\n" +
+            "      ],\n" +
+            "      \"templateName\": \"lens-flare-template.json\",\n" +
+            "      \"templateThumbnailURI\": \"UnleashThePowerOfNatureThumbnail.jpg\"\n" +
+            "    },\n" +
+            "    {\n" +
+            "      \"configuration\": \"accountingtravis.json\",\n" +
+            "      \"id\": \"01DX1RB94P35Q1A2W6AA5XCQZ9\",\n" +
+            "      \"isNew\": false,\n" +
+            "      \"isPremium\": true,\n" +
+            "      \"templateCategories\": [\n" +
+            "        \"01DJ4TM160ETZR0NT4HA2M0ZTK\",\n" +
+            "        \"01DJ4TM161P490A1DZ3AFKXNNF\"\n" +
+            "      ],\n" +
+            "      \"templateName\": \"lightleaks-template.json\",\n" +
+            "      \"templateThumbnailURI\": \"AccountingTravisThumbnail.jpg\"\n" +
+            "    },\n" +
+            "    {\n" +
+            "      \"configuration\": \"yeti.json\",\n" +
+            "      \"id\": \"01EAEFVPZ6MFJEMCA8XB06HB01\",\n" +
+            "      \"isNew\": true,\n" +
+            "      \"isPremium\": true,\n" +
+            "      \"templateCategories\": [\n" +
+            "        \"01DJ4TM160ETZR0NT4HA2M0ZTK\",\n" +
+            "        \"01DJ4TM161P490A1DZ3AFKXNNF\"\n" +
+            "      ],\n" +
+            "      \"templateName\": \"fashion-template.json\",\n" +
+            "      \"templateThumbnailURI\": \"yeti-thumbnail.jpg\"\n" +
+            "    },\n" +
+            "    {\n" +
+            "      \"configuration\": \"BusinessDev.json\",\n" +
+            "      \"id\": \"01DX1RB965Z96AD283559NJT9T\",\n" +
+            "      \"isNew\": false,\n" +
+            "      \"isPremium\": true,\n" +
+            "      \"templateCategories\": [\n" +
+            "        \"01DJ4TM161P490A1DZ3AFKXNNF\",\n" +
+            "        \"01DJ4TM160ETZR0NT4HA2M0ZTK\"\n" +
+            "      ],\n" +
+            "      \"templateName\": \"holistic-template.json\",\n" +
+            "      \"templateThumbnailURI\": \"BusinessDevDefaultThumbnail.jpg\"\n" +
+            "    },\n" +
+            "    {\n" +
+            "      \"configuration\": \"start-living.json\",\n" +
+            "      \"id\": \"01EAEFVXVT6VS2R24GR7Q40XE0\",\n" +
+            "      \"isNew\": true,\n" +
+            "      \"isPremium\": true,\n" +
+            "      \"templateCategories\": [\n" +
+            "        \"01DJ4TM160ETZR0NT4HA2M0ZTK\",\n" +
+            "        \"01DJ4TM161NNZMQQEXSX6DE4QJ\"\n" +
+            "      ],\n" +
+            "      \"templateName\": \"fashion-template.json\",\n" +
+            "      \"templateThumbnailURI\": \"start-living-thumbnail.jpg\"\n" +
+            "    },\n" +
+            "    {\n" +
+            "      \"configuration\": \"beach4july.json\",\n" +
+            "      \"id\": \"01DX1RB94MBAKC6ECYGMKVJT4B\",\n" +
+            "      \"isNew\": false,\n" +
+            "      \"isPremium\": false,\n" +
+            "      \"templateCategories\": [\n" +
+            "        \"01E2846M7HM3B12V0YTJM0M5KF\"\n" +
+            "      ],\n" +
+            "      \"templateName\": \"fashion-template.json\",\n" +
+            "      \"templateThumbnailURI\": \"Beach4JulyThumbnail.jpg\"\n" +
+            "    },\n" +
+            "    {\n" +
+            "      \"configuration\": \"sliderevealboundingbox-happy-independence-day.json\",\n" +
+            "      \"id\": \"01EC34AY3EQTA5294ZRHC6FQMJ\",\n" +
+            "      \"isNew\": true,\n" +
+            "      \"isPremium\": true,\n" +
+            "      \"templateCategories\": [\n" +
+            "        \"01E2846M7HM3B12V0YTJM0M5KF\"\n" +
+            "      ],\n" +
+            "      \"templateName\": \"slide-reveal-boundingbox-template.json\",\n" +
+            "      \"templateThumbnailURI\": \"sliderevealboundingbox-happy-independence-day-thumbnail.jpg\"\n" +
+            "    },\n" +
+            "    {\n" +
+            "      \"configuration\": \"recipes4july.json\",\n" +
+            "      \"id\": \"01DX1RB923YYSRNFQF90TJNP03\",\n" +
+            "      \"isNew\": false,\n" +
+            "      \"isPremium\": true,\n" +
+            "      \"templateCategories\": [\n" +
+            "        \"01E2846M7HM3B12V0YTJM0M5KF\"\n" +
+            "      ],\n" +
+            "      \"templateName\": \"fashion-template.json\",\n" +
+            "      \"templateThumbnailURI\": \"Recipes4JulyThumbnail.jpg\"\n" +
+            "    },\n" +
+            "    {\n" +
+            "      \"configuration\": \"independence-day-celebration.json\",\n" +
+            "      \"id\": \"01EBX3ZC3HZEXJYKEQ8SZAMGDY\",\n" +
+            "      \"isNew\": true,\n" +
+            "      \"isPremium\": true,\n" +
+            "      \"templateCategories\": [\n" +
+            "        \"01E2846M7HM3B12V0YTJM0M5KF\"\n" +
+            "      ],\n" +
+            "      \"templateName\": \"classic.json\",\n" +
+            "      \"templateThumbnailURI\": \"independence-day-celebration-thumbnail.jpg\"\n" +
+            "    },\n" +
+            "    {\n" +
+            "      \"configuration\": \"happy-valentine's-day.json\",\n" +
+            "      \"id\": \"01E7674TVS66G1AGGMDE3YJVD7\",\n" +
+            "      \"isNew\": false,\n" +
+            "      \"isPremium\": true,\n" +
+            "      \"templateCategories\": [\n" +
+            "        \"01E2846M7HM3B12V0YTJM0M5KF\"\n" +
+            "      ],\n" +
+            "      \"templateName\": \"classic.json\",\n" +
+            "      \"templateThumbnailURI\": \"happy-valentine's-day-thumbnail.jpg\"\n" +
+            "    }\n" +
+            "  ]\n" +
+            "}\n")?.itemDto?.toEntities()?.toFeedItems() ?: emptyList()
+}
